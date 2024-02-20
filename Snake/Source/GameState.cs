e59226a0ebb05609d00d38127dc0788bef4323e7
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-20 14:03:05 +0000

    Created GameState. cs

---
+++ Snake/Source/GameState.cs
@@ -0,0 +1,27 @@
+﻿namespace Snake.Source
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+
+        public Direction SnakeDirection { get; private set; }
+        public int Score { get; private set; }  
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> r_snakePositions = new();
+
+        private readonly Random r_random = new();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[Rows, Cols];
+
+            SnakeDirection = Direction.Right;
+
+        }
+    }
+}
--- Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
+++ Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f38c2599fd1816dfa611d05eb6357362768d14f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45f321f8300cbc2dcb4c509b4fb7c9cd73393fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
