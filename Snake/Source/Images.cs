ae956055611ac593e11fb03cab8d2c019550a162
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-21 13:04:28 +0000

    Base of loading images

---
+++ Snake/Source/Images.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Snake.Source
+{
+    public static class Images
+    {
+        public static readonly ImageSource Body = LoadImage("all_sprites-Body.png");
+        public static readonly ImageSource BodyDead = LoadImage("all_sprites-Body_Dead.png");
+        public static readonly ImageSource Empty = LoadImage("all_sprites-Empty.png");
+        public static readonly ImageSource Food = LoadImage("all_sprites-Food.png");
+        public static readonly ImageSource Head = LoadImage("all_sprites-Head.png");
+        public static readonly ImageSource HeadDead = LoadImage("all_sprites-Head_Dead.png");
+
+        private static ImageSource LoadImage(string fileName) 
+            => new BitmapImage(new Uri($"Assets/Sprites/{fileName}", UriKind.Relative));
+    }
+}
--- Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
+++ Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f12d61943fc6afdbbb5068aa29de7734338999f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aeb9616e5cba824d6442069a9ad5c9387b38052")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
