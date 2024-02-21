7f12d61943fc6afdbbb5068aa29de7734338999f
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-21 11:30:07 +0000

    GameState adding food

--- Snake/Source/GameState.cs
+++ Snake/Source/GameState.cs
@@ -23,17 +23,36 @@
             SnakeDirection = Direction.Right;
 
             AddSnake();
+
+            AddFood();
         }
 
         public void AddSnake()
         {
             var r = Rows / 2;
-
             for (var c = 1; c <= 3; c++)
             {
                 Grid[r, c] = GridValue.Snake;
                 r_snakePositions.AddFirst(new Position(r, c));   
             }
+        }
+
+        private IEnumerable<Position> EmptyPositions()
+        {
+            for (var r = 0; r < Rows; r++)
+                for (var c = 0; c < Cols; c++)
+                    if (Grid[r, c] == GridValue.Empty)
+                        yield return new(r, c);
+        }
+
+        private void AddFood()
+        {
+            var emptyPositions = new List<Position>(EmptyPositions());
+            if (emptyPositions.Count == 0)
+                return;
+
+            var pos = emptyPositions[r_random.Next(emptyPositions.Count)];
+            Grid[pos.Row, pos.Col] = GridValue.Food;
         }
     }
 }
