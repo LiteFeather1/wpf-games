aa3d8bb11712f64f3687debef12fa13992f6ea70
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-26 11:40:47 +0000

    Wpf Init

--- Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
+++ Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6586f637aeffe2750f893ab36f3cd43dacecb9e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f30a0a65e3b0d17245407e89b6ed1d5323f49d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
