8182e26801752a918cfed2737d8c314cddc5ad65
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-26 12:33:03 +0000

    Create JBlock.cs

---
+++ Tetris/Source/Blocks/JBlock.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Tetris.Source.Blocks
+{
+    public class JBlock : Block
+    {
+        private readonly Position[][] r_tiles =
+        [
+            [new(0, 0), new(1, 0), new(1, 1), new(1, 2)],
+            [new(0, 1), new(0, 2), new(1, 1), new(2, 1)],
+            [new(1, 0), new(1, 1), new(1, 2), new(2, 2)],
+            [new(0, 1), new(1, 1), new(2, 0), new(2, 1)]
+        ];
+
+        protected override Position[][] Tiles => r_tiles;
+        protected override Position StartOffSet => new(0, 3);
+        public override int ID => 2;
+    }
+}
