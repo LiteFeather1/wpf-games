318129580c85ccbd42224c02a28fbb3f445a0688
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-28 11:50:04 +0000

    Setting up the game canvas

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tetris.Source;
 
 namespace Tetris
 {
@@ -16,9 +17,35 @@ namespace Tetris
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] _imageControls;
+
+        private GameState _gameState;
+
         public MainWindow()
         {
-            //InitializeComponent();
+            InitializeComponent();
+            const int ROWS = 22, COLS = 10;
+
+            _gameState = new(ROWS, COLS);
+
+            // Setup Game Canvas
+            _imageControls = new Image[ROWS, COLS];
+            int cellSize = (int)(GameCanvas.Width / COLS); 
+            for (var r = 0; r < ROWS; r++)
+                for (var c = 0; c < COLS; c++)
+                {
+                    var imageControl = new Image()
+                    {
+                        Width = cellSize,
+                        Height = cellSize,
+                    };
+
+                    // The top 2 rows are invisible/ not visible
+                    Canvas.SetTop(imageControl, (r - 2) * cellSize);
+                    Canvas.SetLeft(imageControl, c * cellSize);
+                    GameCanvas.Children.Add(imageControl);
+                    _imageControls[r, c] = imageControl;
+                }
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -27,6 +54,11 @@ namespace Tetris
         }
 
         private void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
