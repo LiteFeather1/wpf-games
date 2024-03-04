b0e8085e904d66c0b50c8121b625883d8f2e8f46
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-03-04 13:15:34 +0000

    Base of GameState logic

---
+++ Tic-Tac-Toe/Source/GameState.cs
@@ -0,0 +1,27 @@
+﻿using Tic_Tac_Toe.Source.Enums;
+
+namespace Tic_Tac_Toe.Source;
+
+public class GameState
+{
+    public Player[,] GameGrid { get; private set; }
+
+    public Player CurrentPlayer { get; private set; }
+
+    public int TurnsPassed { get; private set; }
+
+    public bool GameOver { get; private set; }
+
+    public delegate void MoveMade(int row, int collum);
+    public event MoveMade OnMoveMade;
+
+    public event Action<GameResult> GameEnded;
+
+    public event Action GameRestarted;
+
+    public GameState()
+    {
+        GameGrid = new Player[3, 3];
+        CurrentPlayer = Player.X;
+    }
+}
