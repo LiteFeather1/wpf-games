85f52d6ce6c1aa70ad0fcf247c5114126b078a54
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-03-04 14:10:46 +0000

    Create SquareCoordinate.cs

---
+++ Tic-Tac-Toe/Source/SquareCoordinate.cs
@@ -0,0 +1,7 @@
+﻿namespace Tic_Tac_Toe.Source;
+
+public readonly struct SquareCoordinate(int row, int col)
+{
+    public readonly int Row { get; } = row;
+    public readonly int Col { get; } = col;
+}
