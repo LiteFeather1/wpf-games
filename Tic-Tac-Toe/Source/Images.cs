61ba7069e5f552b8aa46171bbe8631fb449c741a
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-03-05 13:23:59 +0000

    Create Images.cs

---
+++ Tic-Tac-Toe/Source/Images.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media.Imaging;
+using Tic_Tac_Toe.Source.Enums;
+
+namespace Tic_Tac_Toe.Source;
+
+public class Images
+{
+    public readonly Dictionary<Player, BitmapImage> ImageSources = new()
+    {
+        { Player.X, new(new("Pack://application:,,,/Assets/Xs/X_15.png")) },
+        { Player.O, new(new("Pack://application:,,,/Assets/Os/O_15.png")) },
+    };
+}
