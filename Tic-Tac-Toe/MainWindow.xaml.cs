8672d1b57a88171e3c9d8f87973429bb8d9a81bd
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-03-05 13:24:24 +0000

    Loading resource colours on MainWindow

--- Tic-Tac-Toe/MainWindow.xaml.cs
+++ Tic-Tac-Toe/MainWindow.xaml.cs
@@ -16,9 +16,29 @@ namespace Tic_Tac_Toe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly SolidColorBrush sr_xColour;
+        private readonly SolidColorBrush sr_oColour;
+        private readonly Color sr_xDropShadowColour;
+        private readonly Color sr_oDropShadowColour;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            sr_oColour = Resources["OColour"] as SolidColorBrush;
+            sr_xColour = Resources["XColour"] as SolidColorBrush;
+            sr_oDropShadowColour = (Color)Resources["ODropShadowColour"];
+            sr_oDropShadowColour = (Color)Resources["XDropShadowColour"];
+        }
+
+        private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
