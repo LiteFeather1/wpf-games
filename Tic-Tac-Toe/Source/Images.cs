f997d3334afe25f9604b3e96ed6c56701c2b9d96
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-03-05 13:51:58 +0000

    Images Class is clear

--- Tic-Tac-Toe/Source/Images.cs
+++ Tic-Tac-Toe/Source/Images.cs
@@ -3,11 +3,13 @@ using Tic_Tac_Toe.Source.Enums;
 
 namespace Tic_Tac_Toe.Source;
 
-public class Images
+public static class Images
 {
-    public readonly Dictionary<Player, BitmapImage> ImageSources = new()
+    private static readonly Dictionary<Player, BitmapImage> r_playerCompleteMessages = new()
     {
         { Player.X, new(new("Pack://application:,,,/Assets/Xs/X_15.png")) },
         { Player.O, new(new("Pack://application:,,,/Assets/Os/O_15.png")) },
     };
+
+    public static Dictionary<Player, BitmapImage> ImageSources = r_playerCompleteMessages;
 }
